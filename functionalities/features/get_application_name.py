df0ee079182fab372b1f13c185b95e9a5ac1ae3b
Rahul <96227426+2Rahul2@users.noreply.github.com>
2024-08-12 10:55:42 +0530

    changed get_application file

--- functionalities/features/get_application_name.py
+++ functionalities/features/get_application_name.py
@@ -1,6 +1,9 @@
 import psutil
 from functionalities.features.check_similarity import similar
-from AppOpener import open
+try:
+    from AppOpener import open
+except:
+    pass
 
 class Application:
     def __init__(self):
@@ -10,36 +13,44 @@ class Application:
         self.stop_words = ["the", "a", "an", "please"]
 
     def extract_application_name(self , text):
-        concat_words = self.action_keywords+self.app_keywords+self.stop_words+self.close_action_keywords
-        filtered_lst = []
-        for word in text.split():
-            if word not in concat_words:
-                filtered_lst.append(word)
-        return " ".join(filtered_lst)
+        try:
+            concat_words = self.action_keywords+self.app_keywords+self.stop_words+self.close_action_keywords
+            filtered_lst = []
+            for word in text.split():
+                if word not in concat_words:
+                    filtered_lst.append(word)
+            return " ".join(filtered_lst)
+        except:
+            pass
     
     def open_application(self,text):
-        app_name = self.extract_application_name(text)
-        open(app_name ,match_closest=True)
-        print(f"opening application : {app_name.replace(' ' ,'')}.exe")
-
+        try:
+            app_name = self.extract_application_name(text)
+            open(app_name ,match_closest=True)
+            print(f"opening application : {app_name.replace(' ' ,'')}.exe")
+        except:
+            pass
     def close_application(self , text):
-        app_name = self.extract_application_name(text)
-        similar_object = similar(app_name.lower())
-        print(f"closing application : {app_name.replace(' ' ,'')}.exe")
-        for proc in psutil.process_iter(['pid', 'name']):
-            try:
+        try:
+            app_name = self.extract_application_name(text)
+            similar_object = similar(app_name.lower())
+            print(f"closing application : {app_name.replace(' ' ,'')}.exe")
+            for proc in psutil.process_iter(['pid', 'name']):
+                try:
 
-                is_similiar ,val = similar_object.checkSimilar(proc.info['name'].lower() ,0.75)
-                print(app_name.lower() , proc.info['name'].lower() ,val)
-                # Check if the process name matches the given application name
-                if is_similiar:
-                # if app_name.lower() in proc.info['name'].lower():
-                    # Terminate the process
-                    print(f"{proc.info['name'].lower()} (PID {proc.info['pid']}) terminated successfully.")
-                    proc.terminate()
-            except Exception as e:
-                print("cant find application" ,e)
-                pass
+                    is_similiar ,val = similar_object.checkSimilar(proc.info['name'].lower() ,0.75)
+                    print(app_name.lower() , proc.info['name'].lower() ,val)
+                    # Check if the process name matches the given application name
+                    if is_similiar:
+                    # if app_name.lower() in proc.info['name'].lower():
+                        # Terminate the process
+                        print(f"{proc.info['name'].lower()} (PID {proc.info['pid']}) terminated successfully.")
+                        proc.terminate()
+                except Exception as e:
+                    print("cant find application" ,e)
+                    pass
+        except:
+            pass
 
 if __name__ == "__main__":
     app = Application()
